e50ce404e1946553d9751f7d28e27e246f224d56
kfolmer1010 <karenfolmer@hotmail.com>
2024-03-22 19:42:58 -0300

    Se sube ejercicio 4 de la Guia 1

---
+++ firmware/projects/Guia_1_EJ3/main/Guia_1_EJ3.c
@@ -0,0 +1,111 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+/*==================[macros and definitions]=================================*/
+#define ON 1
+#define OFF 0
+#define TOGGLE 3
+#define CONFIG_BLINK_PERIOD 100
+/*==================[internal data definition]===============================*/
+struct leds
+{
+    uint8_t mode;       //ON, OFF, TOGGLE
+	uint8_t n_led;        //indica el número de led a controlar
+	uint8_t n_ciclos;   //indica la cantidad de ciclos de ncendido/apagado
+	uint16_t periodo;    //indica el tiempo de cada ciclo
+} my_leds; 
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+
+void ControlLeds(struct leds *leds_ptr)
+{
+	switch (leds_ptr->mode)
+	{
+	case ON:
+		if (leds_ptr->n_led == LED_1){
+			LedOn(LED_1);
+		}
+			
+		else if(leds_ptr->n_led == LED_2){
+				LedOn(LED_2);
+		}
+		else if(leds_ptr->n_led == LED_3){
+				LedOn(LED_3);
+		}	
+		
+		break;
+	
+	// default:
+	// 	break;
+	case OFF:
+		if(leds_ptr->n_led == LED_1)
+				LedOff(LED_1);
+			else if(leds_ptr->n_led == LED_2)
+				LedOff(LED_2);
+			else if(leds_ptr->n_led == LED_3)
+				LedOff(LED_3);
+		
+		break;
+	
+	// default:
+	// 	break;
+
+	case TOGGLE:
+		for(int i=0; i<leds_ptr->n_ciclos; i++){
+			if(leds_ptr->n_led == LED_1)
+				LedToggle(LED_1);
+			else if(leds_ptr->n_led == LED_2)
+				LedToggle(LED_2);
+			else if(leds_ptr->n_led == LED_3)
+				LedToggle(LED_3);
+			for(int j=0; j<leds_ptr->periodo; j++){
+						vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+
+			}
+		}		
+			
+		break;
+	}
+}
+
+void app_main(void){
+	LedsInit();
+	my_leds.mode = 3;
+	my_leds.n_led = LED_1;
+	my_leds.n_ciclos = 10;
+	my_leds.periodo = 5;
+	ControlLeds(&my_leds);
+}
+
+
+
+
---
+++ firmware/projects/Guia_1_EJ4/main/Guia_1_EJ4.c
@@ -0,0 +1,54 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+/*==================[Actividad]=============================================*/
+/*Escriba una función que reciba un dato de 32 bits,  la cantidad de dígitos de 
+salida y un puntero a un arreglo donde se almacene los n dígitos. La función deberá 
+convertir el dato recibido a BCD, guardando cada uno de los dígitos de salida en el 
+arreglo pasado como puntero.
+*/
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <gpio_mcu.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+void  convertToBcdArray (uint32_t data, uint8_t digits, uint8_t * bcd_number)
+{
+	for(int i=digits-1; i>=0; i--)
+    {
+        bcd_number[i] = data % 10;
+    }
+}
+/*==================[external functions definition]==========================*/
+void app_main(void)
+{
+    uint8_t arreglo[3];
+    convertToBcdArray(456, 3, arreglo);
+}
+/*==================[end of file]============================================*/
