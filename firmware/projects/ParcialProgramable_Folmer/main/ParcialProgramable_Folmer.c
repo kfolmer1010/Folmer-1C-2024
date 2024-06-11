5538c89b949b030c05df2a3ff53db77837cf5178
kfolmer1010 <karenfolmer@hotmail.com>
2024-06-11 09:34:51 -0300

    Subo carpeta donde realizaré el código del parcial

---
+++ firmware/projects/ParcialProgramable_Folmer/main/ParcialProgramable_Folmer.c
@@ -0,0 +1,39 @@
+/*! @mainpage ParcialProgramable_Folmer
+ *
+ * @section genDesc General Description
+ *
+ * El siguiente programa fue implementado con motivo de la realización del parcial de Electrónica Programable.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 11/06/2024 | Document creation		                         |
+ *
+ * @author Karen Folmer (karen.folmer@ingenieria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
