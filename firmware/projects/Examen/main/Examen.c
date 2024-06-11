da45577507616e6998346df4549a1acc731b805c
kfolmer1010 <karenfolmer@hotmail.com>
2024-06-11 11:57:23 -0300

    Subo la definicion de los periodos de sensado y funcion para convertir V a pH

--- firmware/projects/Examen/main/Examen.c
+++ firmware/projects/Examen/main/Examen.c
@@ -1,8 +1,9 @@
-/*! @mainpage Template
+/*! @mainpage Examen
  *
  * @section genDesc General Description
  *
- * This section describes how the program works.
+ * El siguiente programa fue implementado con motivo de la realización del parcial de Electrónica Programable.
+ * El mismo consiste en un sistema de control de pH y humedad. 
  *
  * <a href="https://drive.google.com/...">Operation Example</a>
  *
@@ -17,23 +18,40 @@
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 11/06/2024 | Document creation		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Karen Folmer (karen.folmer@ingenieria.uner.edu.ar)
  *
  */
 
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include "gpio_mcu.h"
+#include "analog_io_mcu.h"
+
 /*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD_SENSADO 3000000
+#define CONFIG_BLINK_PERIOD_INFORME 5000000
+
+#define GPIO_BOMBA_AGUA GPIO_15
+#define GPIO_BOMBA_PHA GPIO_16
+#define GPIO_BOMBA_PHB GPIO_17
+#define GPIO_SENSOR_RIEGO GPIO_18
+#define GPIO_SENSOR_PH GPIO_19
 
 /*==================[internal data definition]===============================*/
-
+uint16_t valor_sensado, data; 
 /*==================[internal functions declaration]=========================*/
+void ValorSensado()
+{
+	AnalogInputReadSingle(CH1, &valor_sensado);
+	data = valor_sensado*14/3; 
+}
 
 /*==================[external functions definition]==========================*/
-void app_main(void){
-	printf("Hello world!\n");
+void app_main(void)
+{
+	
 }
 /*==================[end of file]============================================*/
